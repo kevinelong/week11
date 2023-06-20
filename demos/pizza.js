0e5c3e4ada4ce58fb452b46951ce4508c2733c91
kevinelong <kevinelong@gmail.com>
2023-06-20 12:31:36 -0700

    pizza

---
+++ demos/pizza.js
@@ -0,0 +1,41 @@
+//PIZZA
+class SauceType{ //ENUM ENUMERATION
+    static RED="Red";
+    static WHITE="White";
+    static CLAM="Clam";
+}
+class CheeseType{ //ENUM ENUMERATION
+    static ITALIAN="Italian";
+    static AMERICAN="American";
+}
+class Topping{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+}
+class Mushrooms extends Topping{ // IS A - INHERITANCE
+    constructor(){
+        super("Mushrooms", 1.00)
+    }
+}
+class Pineapple extends Topping{ // IS A - INHERITANCE
+    constructor(){
+        super("Pineapple", 1.00)
+    }
+}
+class Pizza{
+    constructor(){
+        this.cheeseType = CheeseType.ITALIAN; // HAS A - COMPOSITION
+        this.sauceType = SauceType.RED; // HAS A - COMPOSITION
+        this.toppingsList = [] // HAS A  - COMPOSITION
+    }
+}
+p = new Pizza()
+p.toppingsList.push(new Mushrooms())
+p.toppingsList.push(new Pineapple())
+
+console.log(p.sauceType)
+console.log(p.toppingsList)
+
+
